f5da076311a57ee04d0f447a164088374ed31696
AtulSisodiya <atulsisodiya.cse22@jecrc.ac.in>
2022-07-04 10:13:13 +0530

    add

--- Shopkart/src/main/java/com/ecommerce/controller/Cart_control.java
+++ Shopkart/src/main/java/com/ecommerce/controller/Cart_control.java
@@ -1,9 +1,12 @@
 package com.ecommerce.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,4 +38,16 @@ public class Cart_control {
 		cartrepo.save(ab);
 		return cartrepo.findAll();
 		}
+	
+	@DeleteMapping("{id}/delete/{id_product}")
+	public List<Cart> removeFromCart(@PathVariable Long id,Long id_product){
+		List<Cart> carts = cartrepo.save(null);
+		System.out.println(carts);
+		
+
+		return null;
+		
+		
+		
+	}
 }
